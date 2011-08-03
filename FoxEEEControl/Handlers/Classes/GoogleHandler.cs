cb036cb6f97d551e1aad51671e0cf5c7518caeca
Doridian Draconia <mriq91@gmail.com>
2011-08-03 21:42:22 +0000

    Ups, forgot a bit ^^;
    git-svn-id: https://svn.doridian.de/foxeeecontrol@15 ed20719d-01f8-4cff-9259-02f86933cedc

---
+++ FoxEEEControl/Handlers/Classes/GoogleHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using FoxEEEControl.Handlers.Types;
+
+namespace FoxEEEControl.Handlers.Classes
+{
+    class GoogleHandler : PrefixHandler
+    {
+        public override string PREFIX { get { return "g"; } }
+
+        protected override HandlerItem[] PrefixGetResultsFor(string search)
+        {
+            List<HandlerItem> ret = new List<HandlerItem>();
+            ret.Add(new HandlerItem("http://www.google.de/search?q=" + search, this));
+            return ret.ToArray();
+        }
+    }
+}
---
+++ FoxEEEControl/Handlers/Types/GenericHandler.cs
@@ -0,0 +1,17 @@
+﻿
+namespace FoxEEEControl.Handlers.Types
+{
+    abstract class GenericHandler
+    {
+        public virtual void Initialize(object param) { } //true = force, false = noforce < atm
+        public abstract HandlerItem[] GetResultsFor(string search);
+        public virtual void Start(string item)
+        {
+            throw new HandlerUseShellExecException();
+        }
+        public virtual string GetTextForItem(string item)
+        {
+            return item;
+        }
+    }
+}
